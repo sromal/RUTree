69e9d611268efd08721ea64fa8a0b2a72b4cbe7e
Maxwell Wang <maxwellwang01@gmail.com>
2019-10-19 17:23:23 -0400

    started flask process

--- TreeMaker.py
+++ TreeMaker.py
@@ -1,10 +1,21 @@
 import requests
+import json
+import graphviz
+import re
 
-subject = "198"
+subject = "830"
 link = "http://sis.rutgers.edu/oldsoc/courses.json?subject=" + subject + "&semester=12020&campus=NB&level=UG"
 data = requests.get(link).text
-
-class Course:
-    name = ""
-    id = ""
-    prereqs = []
+courses = json.loads(data)
+dot = graphviz.Digraph(comment='Tree')
+for course in courses:
+    dot.node(course["courseNumber"], course["courseNumber"] + " " + course["title"])
+    prereqs = re.findall("\\d+:\\d+:\\d+", str(course["preReqNotes"]))
+    masterPrereq = None
+    for prereq in prereqs:
+        if prereq[3:6] == subject:
+            masterPrereq = prereq[7:]
+            break
+    if not masterPrereq == None:
+        dot.edge(masterPrereq, course["courseNumber"])
+dot.render('trees/'+subject+'.gv', view=True)
