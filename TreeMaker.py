3cb469f1899e88b2383659228bad6024bc3c506c
Maxwell Wang <maxwellwang01@gmail.com>
2019-10-19 18:39:29 -0400

    increased dependency accuracy

--- TreeMaker.py
+++ TreeMaker.py
@@ -3,19 +3,24 @@ import json
 import graphviz
 import re
 
-subject = "830"
+subject = "198"
 link = "http://sis.rutgers.edu/oldsoc/courses.json?subject=" + subject + "&semester=12020&campus=NB&level=UG"
 data = requests.get(link).text
 courses = json.loads(data)
 dot = graphviz.Digraph(comment='Tree')
+
 for course in courses:
     dot.node(course["courseNumber"], course["courseNumber"] + " " + course["title"])
     prereqs = re.findall("\\d+:\\d+:\\d+", str(course["preReqNotes"]))
-    masterPrereq = None
+
     for prereq in prereqs:
         if prereq[3:6] == subject:
-            masterPrereq = prereq[7:]
+            dot.edge(prereq[7:], course["courseNumber"])
+            end = course["preReqNotes"].index(prereq[7:])
+            start = course["preReqNotes"].find("and", end)
+            possible = course["preReqNotes"][start + 5:start + 15]
+            if possible[3:6] == subject:
+                dot.edge(possible[7:], course["courseNumber"])
             break
-    if not masterPrereq == None:
-        dot.edge(masterPrereq, course["courseNumber"])
-dot.render('trees/'+subject+'.gv', view=True)
+
+dot.render('trees/' + subject + '.gv', view=True)
